600fa7063c27766a97de139a06fb54b2e6f6cbac
Gustavo Rodriguez <grnarvaez76@gmail.com>
2025-11-05 11:29:26 -0600

    Refactorización de acceso a base de datos
    Se ha cambiado la cadena de conexión en `DatabaseHelper` a `II-46ConnectionString`.
    
    En `dbPersona`, se ha añadido una instancia de `DatabaseHelper` como campo privado `dbHelper`.
    
    Los métodos `Create` y de eliminación en `dbPersona` ahora utilizan `dbHelper.ExecuteNonQuery` en lugar de manejar manualmente la conexión y el comando SQL. El código anterior ha sido comentado.

--- db/dbPersona.vb
+++ db/dbPersona.vb
@@ -2,6 +2,7 @@
 
 Public Class dbPersona
     Public ReadOnly connectionString As String = ConfigurationManager.ConnectionStrings("II-46ConnectionString").ConnectionString
+    Private ReadOnly dbHelper = New DatabaseHelper() ' Instancia de DatabaseHelper
 
     Public Function Create(Persona As Persona) As String
         Try
@@ -14,15 +15,15 @@ Public Class dbPersona
                 New SqlParameter("@FechaNacimiento", Persona.FechaNacimiento),
                 New SqlParameter("@Telefono", Persona.Telefono)
             }
+            dbHelper.ExecuteNonQuery(sql, parametros)
+            'Using connection As New SqlConnection(connectionString)
+            '    Using command As New SqlCommand(sql, connection)
+            '        command.Parameters.AddRange(parametros.ToArray())
+            '        connection.Open()
+            '        command.ExecuteNonQuery()
+            '    End Using
 
-            Using connection As New SqlConnection(connectionString)
-                Using command As New SqlCommand(sql, connection)
-                    command.Parameters.AddRange(parametros.ToArray())
-                    connection.Open()
-                    command.ExecuteNonQuery()
-                End Using
-
-            End Using
+            'End Using
         Catch ex As Exception
 
         End Try
@@ -36,14 +37,15 @@ Public Class dbPersona
             Dim parametros As New List(Of SqlParameter) From {
                 New SqlParameter("@IdPersona", idPersona)
             }
+            dbHelper.ExecuteNonQuery(sql, parametros)
 
-            Using connection As New SqlConnection(connectionString)
-                Using command As New SqlCommand(sql, connection)
-                    command.Parameters.AddRange(parametros.ToArray())
-                    connection.Open()
-                    command.ExecuteNonQuery()
-                End Using
-            End Using
+            'Using connection As New SqlConnection(connectionString)
+            '    Using command As New SqlCommand(sql, connection)
+            '        command.Parameters.AddRange(parametros.ToArray())
+            '        connection.Open()
+            '        command.ExecuteNonQuery()
+            '    End Using
+            'End Using
 
             Return "Persona eliminada correctamente"
         Catch ex As Exception
